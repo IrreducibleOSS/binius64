99dac3f7956c9e0a7b54165ffde5e6cc98881109
pepyakin <sshulepov@irreducible.com>
2025-07-01 16:26:02 +0000

    [chore] rustfmt (#23)

--- crates/prover/src/prove.rs
+++ crates/prover/src/prove.rs
@@ -1,8 +1,9 @@
-use super::error::Error;
 use binius_field::{BinaryField, ExtensionField};
 use binius_frontend::constraint_system::{ConstraintSystem, ValueVec};
 use binius_transcript::{ProverTranscript, fiat_shamir::Challenger};
 use binius_verifier::{Params, fields::B64};
+
+use super::error::Error;
 
 pub fn prove<F, Challenger_>(
 	params: &Params,
