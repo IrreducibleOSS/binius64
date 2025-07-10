3b5c8f25efef01d6db004e9b85ee9e183ecee40f
jimpo <881253+jimpo@users.noreply.github.com>
2025-07-10 09:05:22 +0000

    [arith-bench] Clean up benchmarks (#91)
    Remove many cfg gates.

--- crates/arith-bench/benches/field_mul.rs
+++ crates/arith-bench/benches/field_mul.rs
@@ -1,22 +1,23 @@
-#[cfg(target_arch = "x86_64")]
-#[allow(unused_imports)]
-use std::arch::x86_64::{__m128i, __m256i};
-#[cfg(target_arch = "x86_64")]
-use std::{array, hint::black_box};
+#![allow(dead_code)]
+#![allow(unused_imports)]
 
-#[cfg(target_arch = "x86_64")]
+use std::{
+	arch::x86_64::{__m128i, __m256i},
+	array,
+	hint::black_box,
+};
+
 use binius_arith_bench::Underlier;
-#[cfg(target_arch = "x86_64")]
-use criterion::{BenchmarkGroup, Throughput};
-use criterion::{Criterion, criterion_group, criterion_main};
-#[cfg(target_arch = "x86_64")]
+use criterion::{BenchmarkGroup, Criterion, Throughput, criterion_group, criterion_main};
 use rand::{
 	Rng,
 	distr::{Distribution, StandardUniform},
 };
 
-#[cfg(target_arch = "x86_64")]
-#[allow(dead_code)]
+/// Runs the field throughput benchmark strategy in the Longfellow-ZK repo.
+///
+/// This increases the density of field multiplications compared to the amount of memory used per
+/// iteration.
 fn run_google_mul_benchmark<U, R>(
 	group: &mut BenchmarkGroup<'_, criterion::measurement::WallTime>,
 	name: &str,
@@ -61,9 +62,7 @@ fn run_google_mul_benchmark<U, R>(
 	});
 }
 
-/// Generic benchmark helper for multiplication operations
-#[cfg(target_arch = "x86_64")]
-#[allow(dead_code)]
+/// Generic benchmark helper for multiplication operations.
 fn run_mul_benchmark<T, R>(
 	group: &mut BenchmarkGroup<'_, criterion::measurement::WallTime>,
 	name: &str,
@@ -93,420 +92,352 @@ fn run_mul_benchmark<T, R>(
 }
 
 /// Benchmark GF(2^8) multiplication using GFNI instructions
-fn bench_rijndael_mul(c: &mut Criterion) {
-	#[cfg(not(target_arch = "x86_64"))]
+#[allow(unused_variables, unused_mut)]
+fn bench_rijndael(c: &mut Criterion) {
+	use binius_arith_bench::rijndael::mul_gfni;
+
+	let mut group = c.benchmark_group("rijndael");
+	let mut rng = rand::rng();
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "gfni", target_feature = "sse2"))]
 	{
-		let _ = c; // Suppress unused variable warning
+		run_mul_benchmark(
+			&mut group,
+			"mul_gfni::<__m128i>",
+			mul_gfni::<__m128i>,
+			&mut rng,
+			8,
+			__m128i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	// Benchmark __m256i
+	#[cfg(all(target_feature = "gfni", target_feature = "avx"))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::rijndael::mul_gfni;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("rijndael_mul_gfni");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "gfni", target_feature = "sse2"))]
-		{
-			run_mul_benchmark(&mut group, "m128i", mul_gfni::<__m128i>, &mut rng, 8, __m128i::BITS);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(target_feature = "gfni", target_feature = "avx"))]
-		{
-			run_mul_benchmark(&mut group, "m256i", mul_gfni::<__m256i>, &mut rng, 8, __m256i::BITS);
-		}
-
-		group.finish();
+		run_mul_benchmark(
+			&mut group,
+			"mul_gfni::<__m256i>",
+			mul_gfni::<__m256i>,
+			&mut rng,
+			8,
+			__m256i::BITS,
+		);
 	}
+
+	group.finish();
 }
 
 /// Benchmark GF(2^128) polynomial Montgomery multiplication using CLMUL instructions
-fn bench_polyval_mul(c: &mut Criterion) {
-	#[cfg(not(target_arch = "x86_64"))]
+#[allow(unused_imports, unused_variables, unused_mut)]
+fn bench_polyval(c: &mut Criterion) {
+	use binius_arith_bench::polyval::mul_clmul;
+
+	let mut rng = rand::rng();
+
+	let mut group = c.benchmark_group("polyval");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		let _ = c; // Suppress unused variable warning
+		run_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m128i>",
+			mul_clmul::<__m128i>,
+			&mut rng,
+			128,
+			__m128i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::polyval::mul_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("polyval_google_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_clmul::<__m128i>,
-				&mut rng,
-				128,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_clmul::<__m256i>,
-				&mut rng,
-				128,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m256i>",
+			mul_clmul::<__m256i>,
+			&mut rng,
+			128,
+			__m256i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	group.finish();
+
+	let mut group = c.benchmark_group("polyval_google_mul_clmul");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::polyval::mul_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("polyval_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_clmul::<__m128i>,
-				&mut rng,
-				128,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_clmul::<__m256i>,
-				&mut rng,
-				128,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_google_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m128i>",
+			mul_clmul::<__m128i>,
+			&mut rng,
+			128,
+			__m128i::BITS,
+		);
 	}
+
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
+	{
+		run_google_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m256i>",
+			mul_clmul::<__m256i>,
+			&mut rng,
+			128,
+			__m256i::BITS,
+		);
+	}
+
+	group.finish();
 }
 
 /// Benchmark GF(2^128) GHASH multiplication using CLMUL instructions
-fn bench_ghash_mul(c: &mut Criterion) {
-	#[cfg(not(target_arch = "x86_64"))]
+#[allow(unused_imports, unused_variables, unused_mut)]
+fn bench_ghash(c: &mut Criterion) {
+	use binius_arith_bench::ghash::mul_clmul;
+
+	let mut rng = rand::rng();
+
+	let mut group = c.benchmark_group("ghash_mul_clmul");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		let _ = c; // Suppress unused variable warning
+		run_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m128i>",
+			mul_clmul::<__m128i>,
+			&mut rng,
+			128,
+			__m128i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::ghash::mul_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("ghash_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_clmul::<__m128i>,
-				&mut rng,
-				128,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_clmul::<__m256i>,
-				&mut rng,
-				128,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m256i>",
+			mul_clmul::<__m256i>,
+			&mut rng,
+			128,
+			__m256i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	group.finish();
+
+	let mut group = c.benchmark_group("ghash_google_mul_clmul");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::ghash::mul_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("ghash_google_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_clmul::<__m128i>,
-				&mut rng,
-				128,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_clmul::<__m256i>,
-				&mut rng,
-				128,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_google_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m128i>",
+			mul_clmul::<__m128i>,
+			&mut rng,
+			128,
+			__m128i::BITS,
+		);
 	}
+
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
+	{
+		run_google_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m256i>",
+			mul_clmul::<__m256i>,
+			&mut rng,
+			128,
+			__m256i::BITS,
+		);
+	}
+
+	group.finish();
 }
 
 /// Benchmark GF(2^64) Monbijou multiplication using CLMUL instructions
-fn bench_monbijou_mul(c: &mut Criterion) {
-	#[cfg(not(target_arch = "x86_64"))]
+#[allow(unused_variables, unused_mut)]
+fn bench_monbijou(c: &mut Criterion) {
+	use binius_arith_bench::monbijou::mul_clmul;
+
+	let mut rng = rand::rng();
+
+	let mut group = c.benchmark_group("monbijou_mul_clmul");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		let _ = c; // Suppress unused variable warning
+		run_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m128i>",
+			mul_clmul::<__m128i>,
+			&mut rng,
+			64,
+			__m128i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::monbijou::mul_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("monbijou_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_clmul::<__m128i>,
-				&mut rng,
-				64,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_clmul::<__m256i>,
-				&mut rng,
-				64,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_mul_benchmark(
+			&mut group,
+			"mul_clmul::<__m256i>",
+			mul_clmul::<__m256i>,
+			&mut rng,
+			64,
+			__m256i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	group.finish();
+
+	let mut group = c.benchmark_group("monbijou_google");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::monbijou::mul_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("monbijou_google_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_clmul::<__m128i>,
-				&mut rng,
-				64,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_clmul::<__m256i>,
-				&mut rng,
-				64,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_google_mul_benchmark(
+			&mut group,
+			"m128i",
+			mul_clmul::<__m128i>,
+			&mut rng,
+			64,
+			__m128i::BITS,
+		);
 	}
+
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
+	{
+		run_google_mul_benchmark(
+			&mut group,
+			"m256i",
+			mul_clmul::<__m256i>,
+			&mut rng,
+			64,
+			__m256i::BITS,
+		);
+	}
+
+	group.finish();
 }
 
 /// Benchmark GF(2^128) Monbijou 128-bit extension field multiplication using CLMUL instructions
-fn bench_monbijou_128b_mul(c: &mut Criterion) {
-	#[cfg(not(target_arch = "x86_64"))]
+#[allow(unused_imports, unused_variables, unused_mut)]
+fn bench_monbijou_128b(c: &mut Criterion) {
+	use binius_arith_bench::monbijou::mul_128b_clmul;
+
+	let mut rng = rand::rng();
+
+	let mut group = c.benchmark_group("monbijou_128b");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		let _ = c; // Suppress unused variable warning
+		run_mul_benchmark(
+			&mut group,
+			"mul_128b_clmul::<__m128i>",
+			mul_128b_clmul::<__m128i>,
+			&mut rng,
+			128,
+			__m128i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::monbijou::mul_128b_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("monbijou_128b_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_128b_clmul::<__m128i>,
-				&mut rng,
-				128,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_128b_clmul::<__m256i>,
-				&mut rng,
-				128,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_mul_benchmark(
+			&mut group,
+			"mul_128b_clmul::<__m256i>",
+			mul_128b_clmul::<__m256i>,
+			&mut rng,
+			128,
+			__m256i::BITS,
+		);
 	}
 
-	#[cfg(target_arch = "x86_64")]
+	group.finish();
+
+	let mut group = c.benchmark_group("monbijou_128b_google");
+
+	// Benchmark __m128i
+	#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
 	{
-		#[allow(unused_imports, unused_variables, unused_mut)]
-		use binius_arith_bench::monbijou::mul_128b_clmul;
-
-		#[allow(unused_variables, unused_mut)]
-		let mut group = c.benchmark_group("monbijou_128b_google_mul_clmul");
-		#[allow(unused_variables, unused_mut)]
-		let mut rng = rand::rng();
-
-		// Benchmark __m128i
-		#[cfg(all(target_feature = "pclmulqdq", target_feature = "sse2"))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m128i",
-				mul_128b_clmul::<__m128i>,
-				&mut rng,
-				128,
-				__m128i::BITS,
-			);
-		}
-
-		// Benchmark __m256i
-		#[cfg(all(
-			target_feature = "vpclmulqdq",
-			target_feature = "avx2",
-			target_feature = "sse2"
-		))]
-		{
-			run_google_mul_benchmark(
-				&mut group,
-				"m256i",
-				mul_128b_clmul::<__m256i>,
-				&mut rng,
-				128,
-				__m256i::BITS,
-			);
-		}
-
-		group.finish();
+		run_google_mul_benchmark(
+			&mut group,
+			"mul_128b_clmul::<__m128i>",
+			mul_128b_clmul::<__m128i>,
+			&mut rng,
+			128,
+			__m128i::BITS,
+		);
 	}
+
+	// Benchmark __m256i
+	#[cfg(all(
+		target_feature = "vpclmulqdq",
+		target_feature = "avx2",
+		target_feature = "sse2"
+	))]
+	{
+		run_google_mul_benchmark(
+			&mut group,
+			"mul_128b_clmul::<__m256i>",
+			mul_128b_clmul::<__m256i>,
+			&mut rng,
+			128,
+			__m256i::BITS,
+		);
+	}
+
+	group.finish();
 }
 
 criterion_group!(
 	benches,
-	bench_rijndael_mul,
-	bench_polyval_mul,
-	bench_ghash_mul,
-	bench_monbijou_mul,
-	bench_monbijou_128b_mul
+	bench_rijndael,
+	bench_polyval,
+	bench_ghash,
+	bench_monbijou,
+	bench_monbijou_128b,
 );
 criterion_main!(benches);
