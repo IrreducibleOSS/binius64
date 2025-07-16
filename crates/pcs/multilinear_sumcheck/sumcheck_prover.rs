1d88380a0a8e8cf516cd15982a6bed8963e59839
jkirmayer <jkirmayer@irreducible.com>
2025-07-16 13:29:39 +0200

    Remove L and L_PRIME constants (#142)
    * bug fix
    
    * fmt
    
    ---------
    
    Co-authored-by: jsk11235 <kirmayerjacob@gmail.com>

--- crates/pcs/multilinear_sumcheck/sumcheck_prover.rs
+++ crates/pcs/multilinear_sumcheck/sumcheck_prover.rs
@@ -1,9 +1,9 @@
 use binius_field::Field;
 
 pub trait SumcheckProver<F: Field> {
-    fn fold(&mut self, challenge: F);
+	fn fold(&mut self, challenge: F);
 
-    fn round_message(&self) -> Vec<F>;
+	fn round_message(&self) -> Vec<F>;
 
-    fn final_eval_claims(self) -> Vec<F>;
+	fn final_eval_claims(self) -> Vec<F>;
 }
