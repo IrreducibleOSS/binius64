7fc0ed82ce9306c06b309728c6d2e9b09f234e7d
Hunter Lindauer <holindauer@gmail.com>
2025-07-17 18:27:42 +0200

    remove rs eq from verifier

--- crates/verifier/src/pcs/verifier.rs
+++ crates/verifier/src/pcs/verifier.rs
@@ -6,7 +6,6 @@ use binius_transcript::{
 use binius_utils::DeserializeBytes;
 use crate::{fields::B1, fri::FRIParams, merkle_tree::MerkleTreeScheme};
 use itertools::Itertools;
-
 use crate::{
 	basefold::verifier::BaseFoldVerifier,
     basefold::utils::{
@@ -25,34 +24,19 @@ use crate::{
 
 use binius_math::tensor_algebra::TensorAlgebra;
 
-// use std::iter;
+use std::iter;
 
 // use binius_field::{ExtensionField, Field, PackedExtension};
 // use binius_verifier::fields::B1;
 
 // use super::tensor_algebra::TensorAlgebra;
 
-pub fn eval_rs_eq<BF: Field + ExtensionField<B1> + PackedExtension<B1>>(
-	z_vals: &[BF],
-	query: &[BF],
-	expanded_row_batch_query: &[BF],
-) -> BF {
-	let tensor_eval = iter::zip(z_vals, query).fold(
-		<TensorAlgebra<B1, BF>>::from_vertical(BF::ONE),
-		|eval, (&vert_i, &hztl_i)| {
-			// This formula is specific to characteristic 2 fields
-			// Here we know that $h v + (1 - h) (1 - v) = 1 + h + v$.
-			let vert_scaled = eval.clone().scale_vertical(vert_i);
 
-			// println!("vert scaled: {:?}", vert_scaled);
-			let hztl_scaled = eval.clone().scale_horizontal(hztl_i);
-			// println!("hztl scaled: {:?}", hztl_scaled);
-
-			eval + &vert_scaled + &hztl_scaled
-		},
-	);
-
-	tensor_eval.fold_vertical(expanded_row_batch_query)
+pub fn compute_mle_eq_sum<BigField: Field>(
+	mle_values: &[BigField],
+	eq_values: &[BigField],
+) -> BigField {
+	mle_values.iter().zip(eq_values).map(|(m, e)| *m * *e).sum()
 }
 
 const KAPPA: usize = 7;
