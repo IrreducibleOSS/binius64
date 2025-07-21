7b1fa4eedb8aef6445243c5b532c14b7bad70f3a
Hunter Lindauer <holindauer@gmail.com>
2025-07-21 14:59:24 +0200

    simplify

--- crates/prover/src/protocols/sumcheck/and_reduction/zerocheck_prover.rs
+++ crates/prover/src/protocols/sumcheck/and_reduction/zerocheck_prover.rs
@@ -1,11 +1,10 @@
-use binius_field::{AESTowerField8b, BinaryField128bPolyval, Field, PackedField};
-use binius_math::{Error, FieldBuffer, multilinear::eq::eq_ind_partial_eval};
+use binius_field::{AESTowerField8b, Field};
+use binius_math::{multilinear::eq::eq_ind_partial_eval};
 use binius_transcript::{
 	ProverTranscript,
 	fiat_shamir::{CanSample, Challenger},
 };
 use itertools::Itertools;
-
 use crate::protocols::sumcheck::{
 	and_reduction::{
 		fold_lookups::precompute_fold_lookup,
@@ -14,7 +13,7 @@ use crate::protocols::sumcheck::{
 		sumcheck_round_message::univariate_round_message,
 		univariate::{
 			delta::delta_poly,
-			ntt_lookup::{NTTLookup, ROWS_PER_HYPERCUBE_VERTEX, SKIPPED_VARS, precompute_lookup},
+			ntt_lookup::{NTTLookup, SKIPPED_VARS},
 			subfield_isomorphism::SubfieldIsomorphismLookup,
 			univariate_poly::{GenericPo2UnivariatePoly, UnivariatePoly},
 		},
@@ -165,21 +164,46 @@ where
 
 #[cfg(test)]
 mod test {
-
-	use binius_field::{
-		AESTowerField8b, AESTowerField128b, BinaryField128bPolyval, Field, PackedBinaryField128x1b,
-		Random,
-	};
+	use binius_field::{AESTowerField8b, AESTowerField128b, BinaryField128bPolyval, Field, PackedBinaryField128x1b, Random};
 	use binius_transcript::ProverTranscript;
 	use binius_verifier::config::StdChallenger;
 	use rand::{SeedableRng, rngs::StdRng};
+	use super::{OblongZerocheckProver};
 
-	use super::{OblongZerocheckProver, *};
+	use crate::protocols::sumcheck::{
+		and_reduction::{
+			one_bit_multivariate::OneBitMultivariate,
+			univariate::{
+				delta::delta_poly,
+				ntt_lookup::{ROWS_PER_HYPERCUBE_VERTEX, SKIPPED_VARS, precompute_lookup},
+				subfield_isomorphism::SubfieldIsomorphismLookup,
+				univariate_poly::{UnivariatePoly},
+			},
+		},
+		common::SumcheckProver,
+	};
+
+	fn random_mlv(log_num_rows: usize, num_polys: usize) -> Vec<OneBitMultivariate> {
+		let mut rng = StdRng::from_seed([0; 32]);
+
+		let mut vec = Vec::with_capacity(num_polys);
+		for _ in 0..num_polys {
+			vec.push(
+				OneBitMultivariate {
+					log_num_rows,
+					packed_evals: (0..1 << log_num_rows).map(|_| PackedBinaryField128x1b::random(&mut rng)).collect(),
+				}
+			);
+		}
+
+		vec
+	}
 
 	#[test]
 	fn verify_univariate_round() {
 		let log_num_rows = 10;
 		let mut rng = StdRng::from_seed([0; 32]);
+
 		let big_field_zerocheck_challenges =
 			vec![BinaryField128bPolyval::random(&mut rng); (log_num_rows - SKIPPED_VARS - 3) + 1];
 
@@ -188,26 +212,9 @@ mod test {
 			AESTowerField8b::new(4),
 			AESTowerField8b::new(16),
 		];
-		let first_mlv = OneBitMultivariate {
-			log_num_rows,
-			packed_evals: (0..1 << log_num_rows)
-				.map(|_| PackedBinaryField128x1b::random(&mut rng))
-				.collect(),
-		};
 
-		let second_mlv = OneBitMultivariate {
-			log_num_rows,
-			packed_evals: (0..1 << log_num_rows)
-				.map(|_| PackedBinaryField128x1b::random(&mut rng))
-				.collect(),
-		};
-
-		let third_mlv = OneBitMultivariate {
-			log_num_rows,
-			packed_evals: (0..1 << log_num_rows)
-				.map(|i| first_mlv.packed_evals[i] * second_mlv.packed_evals[i])
-				.collect(),
-		};
+		let mlvs = random_mlv(log_num_rows, 3);
+		let (first_mlv, second_mlv, third_mlv) = (mlvs[0].clone(), mlvs[1].clone(), mlvs[2].clone());
 
 		let onto_domain: Vec<_> = (ROWS_PER_HYPERCUBE_VERTEX..2 * ROWS_PER_HYPERCUBE_VERTEX)
 			.map(|x| AESTowerField8b::new(x as u8))
@@ -235,6 +242,8 @@ mod test {
 		let prover_poly_terms_on_oblong_hypercube = prover_univariate_message
 			.iter_coeffs()
 			.take(ROWS_PER_HYPERCUBE_VERTEX);
+
+		
 		assert_eq!(
 			prover_poly_terms_on_oblong_hypercube.sum::<BinaryField128bPolyval>(),
 			BinaryField128bPolyval::ZERO
@@ -252,14 +261,17 @@ mod test {
 
 		let delta_mul_by = delta_poly(big_field_zerocheck_challenges[0], SKIPPED_VARS, &iso_lookup)
 			.evaluate_at_challenge(challenge);
+
 		assert_eq!(sumcheck_claim * delta_mul_by, expected_sumcheck_claim);
 	}
 
 	#[test]
 	fn test_transcript_prover_runs() {
+		let mut rng = StdRng::from_seed([0; 32]);
+
 		let mut prover_challenger = ProverTranscript::new(StdChallenger::default());
 		let log_num_rows = 10;
-		let mut rng = StdRng::from_seed([0; 32]);
+
 		let big_field_zerocheck_challenges =
 			vec![BinaryField128bPolyval::random(&mut rng); (log_num_rows - SKIPPED_VARS - 3) + 1];
 
@@ -268,26 +280,9 @@ mod test {
 			AESTowerField8b::new(4),
 			AESTowerField8b::new(16),
 		];
-		let first_mlv = OneBitMultivariate {
-			log_num_rows,
-			packed_evals: (0..1 << log_num_rows)
-				.map(|_| PackedBinaryField128x1b::random(&mut rng))
-				.collect(),
-		};
 
-		let second_mlv = OneBitMultivariate {
-			log_num_rows,
-			packed_evals: (0..1 << log_num_rows)
-				.map(|_| PackedBinaryField128x1b::random(&mut rng))
-				.collect(),
-		};
-
-		let third_mlv = OneBitMultivariate {
-			log_num_rows,
-			packed_evals: (0..1 << log_num_rows)
-				.map(|i| first_mlv.packed_evals[i] * second_mlv.packed_evals[i])
-				.collect(),
-		};
+		let mlvs = random_mlv(log_num_rows, 3);
+		let (first_mlv, second_mlv, third_mlv) = (mlvs[0].clone(), mlvs[1].clone(), mlvs[2].clone());
 
 		let onto_domain: Vec<_> = (ROWS_PER_HYPERCUBE_VERTEX..2 * ROWS_PER_HYPERCUBE_VERTEX)
 			.map(|x| AESTowerField8b::new(x as u8))
