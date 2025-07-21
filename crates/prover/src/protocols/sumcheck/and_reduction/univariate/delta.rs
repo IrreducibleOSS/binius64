c28f12ce3d518d4ce3e1984dbee835c52d843bd9
Hunter Lindauer <holindauer@gmail.com>
2025-07-21 10:47:11 +0200

    univariate utils

---
+++ crates/prover/src/protocols/sumcheck/and_reduction/univariate/delta.rs
@@ -0,0 +1,73 @@
+use binius_field::{arithmetic_traits::InvertOrZero, Field};
+
+use super::{
+    univariate_lagrange::{
+        lexicographic_lagrange_denominator, lexicographic_lagrange_numerators_polyval,
+    },
+    univariate_poly::GenericPo2UnivariatePoly,
+};
+use crate::protocols::sumcheck::and_reduction::univariate::subfield_isomorphism::SubfieldIsomorphismLookup;
+
+pub fn delta_poly<'a,F: Field>(
+    zerocheck_challenge: F,
+    log_degree_lt: usize,
+    iso_lookup: &'a SubfieldIsomorphismLookup<F>,
+) -> GenericPo2UnivariatePoly<'a, F, F> {
+    let _span = tracing::debug_span!("delta_poly").entered();
+
+    let mut univariate_lagrange_coeffs = lexicographic_lagrange_numerators_polyval(
+        1 << log_degree_lt,
+        zerocheck_challenge,
+        iso_lookup,
+    );
+    let lexicographic_lagrange_denominator_inv =
+        lexicographic_lagrange_denominator(log_degree_lt).invert_or_zero();
+
+    for coeff in &mut univariate_lagrange_coeffs {
+        *coeff *= iso_lookup.lookup_8b_value(lexicographic_lagrange_denominator_inv);
+    }
+
+    GenericPo2UnivariatePoly::new(univariate_lagrange_coeffs, iso_lookup)
+}
+
+// #[cfg(test)]
+mod tests {
+    use binius_field::{AESTowerField8b, BinaryField128bPolyval, Field, AESTowerField128b};
+    use crate::{
+        protocols::sumcheck::and_reduction::univariate::{
+            subfield_isomorphism::SubfieldIsomorphismLookup, 
+            univariate_poly::UnivariatePoly,
+            delta::delta_poly,
+        },
+    };
+
+
+    #[test]
+    fn delta_satisfies_definition() {
+        let iso_lookup = SubfieldIsomorphismLookup::<BinaryField128bPolyval>::new::<AESTowerField128b>();
+        for i in 0..64 {
+            let poly = delta_poly(
+                iso_lookup.lookup_8b_value(AESTowerField8b::new(i)),
+                6,
+                &iso_lookup,
+            );
+            for j in 0..64 {
+                if i == j {
+                    assert_eq!(
+                        poly.evaluate_at_challenge(
+                            iso_lookup.lookup_8b_value(AESTowerField8b::new(j))
+                        ),
+                        BinaryField128bPolyval::ONE
+                    );
+                } else {
+                    assert_eq!(
+                        poly.evaluate_at_challenge(
+                            iso_lookup.lookup_8b_value(AESTowerField8b::new(j))
+                        ),
+                        BinaryField128bPolyval::ZERO
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ crates/prover/src/protocols/sumcheck/and_reduction/univariate/univariate_poly.rs
@@ -0,0 +1,151 @@
+use binius_field::{
+    AESTowerField8b, BinaryField128bPolyval, Field, arithmetic_traits::InvertOrZero,
+};
+use crate::protocols::sumcheck::and_reduction::univariate::{
+    subfield_isomorphism::SubfieldIsomorphismLookup,
+    univariate_lagrange::{
+        lexicographic_lagrange_denominator, lexicographic_lagrange_numerators_8b,
+        lexicographic_lagrange_numerators_polyval,
+    },
+};
+
+pub trait UnivariatePoly<FCoeffs: Field, FChallenge: Field> {
+    fn iter_coeffs(&self) -> impl Iterator<Item = &FCoeffs>;
+
+    fn evaluate_at_challenge(&self, challenge: FChallenge) -> FChallenge;
+
+    fn evaluate_at_subfield_point(&self, challenge: FCoeffs) -> FCoeffs;
+
+    fn degree_lt(&self) -> usize;
+}
+
+pub struct GenericPo2UnivariatePoly<'a, F: Field, FChallenge: Field> {
+    univariate_lagrange_coeffs: Vec<F>,
+    log_degree_lt: usize,
+    lookup_table: &'a SubfieldIsomorphismLookup<FChallenge>,
+}
+
+impl<'a, F: Field, FChallenge: Field> GenericPo2UnivariatePoly<'a, F, FChallenge> {
+    pub fn new(
+        univariate_lagrange_coeffs: Vec<F>,
+        iso_lookup: &'a SubfieldIsomorphismLookup<FChallenge>,
+    ) -> Self {
+        let degree_lt = univariate_lagrange_coeffs.len();
+        Self {
+            univariate_lagrange_coeffs,
+            log_degree_lt: degree_lt.trailing_zeros() as usize,
+            lookup_table: iso_lookup,
+        }
+    }
+}
+
+impl UnivariatePoly<AESTowerField8b, BinaryField128bPolyval> for GenericPo2UnivariatePoly<'_, AESTowerField8b, BinaryField128bPolyval> {
+    fn iter_coeffs(&self) -> impl Iterator<Item = &AESTowerField8b> {
+        self.univariate_lagrange_coeffs.iter()
+    }
+
+    fn evaluate_at_challenge(&self, challenge: BinaryField128bPolyval) -> BinaryField128bPolyval {
+        let _span = tracing::debug_span!("evaluate_at_challenge").entered();
+
+        let evals_of_lagrange_basis_vectors_not_yet_divide_by_denominator =
+            lexicographic_lagrange_numerators_polyval(
+                self.degree_lt(),
+                challenge,
+                self.lookup_table,
+            );
+
+        (evals_of_lagrange_basis_vectors_not_yet_divide_by_denominator
+            .iter()
+            .zip(self.iter_coeffs())
+            .map(|(basis_vec_eval, coeff)| {
+                *basis_vec_eval * self.lookup_table.lookup_8b_value(*coeff)
+            })
+            .sum::<BinaryField128bPolyval>())
+            * self.lookup_table.lookup_8b_value(
+                lexicographic_lagrange_denominator(self.log_degree_lt).invert_or_zero(),
+            )
+    }
+
+    fn evaluate_at_subfield_point(&self, challenge: AESTowerField8b) -> AESTowerField8b {
+        let evals_of_lagrange_basis_vectors_not_yet_divide_by_denominator =
+            lexicographic_lagrange_numerators_8b(self.degree_lt(), challenge);
+
+        (evals_of_lagrange_basis_vectors_not_yet_divide_by_denominator
+            .iter()
+            .zip(self.iter_coeffs())
+            .map(|(basis_vec_eval, coeff)| *coeff * *basis_vec_eval)
+            .sum::<AESTowerField8b>())
+            * (lexicographic_lagrange_denominator(self.log_degree_lt).invert_or_zero())
+    }
+
+    fn degree_lt(&self) -> usize {
+        1 << self.log_degree_lt
+    }
+}
+
+impl<F: Field> UnivariatePoly<F, F>
+    for GenericPo2UnivariatePoly<'_, F, F>
+{
+    fn iter_coeffs(&self) -> impl Iterator<Item = &F> {
+        self.univariate_lagrange_coeffs.iter()
+    }
+
+    fn evaluate_at_challenge(&self, challenge: F) -> F {
+        let _span = tracing::debug_span!("evaluate_at_challenge").entered();
+
+        let evals_of_lagrange_basis_vectors_not_yet_divide_by_denominator =
+            lexicographic_lagrange_numerators_polyval(
+                self.degree_lt(),
+                challenge,
+                self.lookup_table,
+            );
+
+        (evals_of_lagrange_basis_vectors_not_yet_divide_by_denominator
+            .iter()
+            .zip(self.iter_coeffs())
+            .map(|(basis_vec_eval, coeff)| *basis_vec_eval * *coeff)
+            .sum::<F>())
+            * self.lookup_table.lookup_8b_value(
+                lexicographic_lagrange_denominator(self.log_degree_lt).invert_or_zero(),
+            )
+    }
+
+    fn evaluate_at_subfield_point(
+        &self,
+        challenge: F,
+    ) -> F {
+        self.evaluate_at_challenge(challenge)
+    }
+
+    fn degree_lt(&self) -> usize {
+        1 << self.log_degree_lt
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use binius_field::{AESTowerField8b, BinaryField128bPolyval, PackedField, AESTowerField128b};
+
+    // use crate::{
+    //     utils::subfield_isomorphism_lookup::SubfieldIsomorphismLookup, univariate::univariate_poly::UnivariatePoly,
+    // };
+
+    // use super::GenericPo2UnivariatePoly;
+
+    #[test]
+    fn univariate_po2_sanity_check() {
+        // let iso_lookup = SubfieldIsomorphismLookup::new::<AESTowerField128b>();
+
+        // let v = (0..64)
+        //     .map(AESTowerField8b::new)
+        //     .map(|x| x.square())
+        //     .collect();
+        // let poly = GenericPo2UnivariatePoly::new(v, &iso_lookup);
+        // for i in 990..1000 {
+        //     assert_eq!(
+        //         poly.evaluate_at_challenge(BinaryField128bPolyval::from(i as u128)),
+        //         BinaryField128bPolyval::from(i as u128).square()
+        //     );
+        // }
+    }
+}
