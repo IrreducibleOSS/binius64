53b4511f7208e82df67f7409db85948b5fedb51e
jsk11235 <kirmayerjacob@gmail.com>
2025-07-21 10:30:44 +0200

    remove print

--- crates/math/src/ring_switch.rs
+++ crates/math/src/ring_switch.rs
@@ -45,9 +45,7 @@ pub fn eval_rs_eq<BF: BinaryField + PackedExtension<B1>>(
 			// Here we know that $h v + (1 - h) (1 - v) = 1 + h + v$.
 			let vert_scaled = eval.clone().scale_vertical(vert_i);
 
-			// println!("vert scaled: {:?}", vert_scaled);
 			let hztl_scaled = eval.clone().scale_horizontal(hztl_i);
-			// println!("hztl scaled: {:?}", hztl_scaled);
 
 			eval + &vert_scaled + &hztl_scaled
 		},
