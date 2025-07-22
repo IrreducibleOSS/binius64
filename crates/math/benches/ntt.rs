70b556db13a059306832a484b9e3e4b59df0bfa4
Johann Birnick <johann.birnick@hotmail.de>
2025-07-22 12:03:20 +0200

    make multiplication throughput available through environment variable

--- crates/math/benches/ntt.rs
+++ crates/math/benches/ntt.rs
@@ -1,22 +1,32 @@
-use std::{
-	iter::repeat_with,
-	time::{Duration, Instant},
-};
+use std::{env::VarError, iter::repeat_with};
 
 use binius_field::{BinaryField, PackedField};
 use binius_math::ntt::{AdditiveNTT, NTTShape, SingleThreadedNTT};
 use binius_utils::rayon::{ThreadPool, ThreadPoolBuilder};
 use criterion::{
 	BenchmarkGroup, BenchmarkId, Criterion, Throughput, criterion_group, criterion_main,
-	measurement::{Measurement, ValueFormatter},
+	measurement::WallTime,
 };
+
+/// `Standard` means it reports the standard input_size / time throughput.
+/// `Multiplication` means it reports num_multiplications / time instead as throughput.
+///
+/// The `Multiplication` variant is useful to compare against the raw multiplication throughput
+/// (from the field benchmarks), so one can see the overhead of the NTT.
+#[derive(Copy, Clone)]
+enum ThroughputVariant {
+	Standard,
+	Multiplication,
+}
 
 /// Benches different NTT implementations with a specific `PackedField` and specific parameter
 /// choice.
 ///
 /// `log_y` is computed automatically from `log_x`, `log_z`, and the size of `data`.
+#[allow(clippy::too_many_arguments)]
 fn bench_ntts<F, P>(
-	group: &mut BenchmarkGroup<WallTimeMulThroughput>,
+	group: &mut BenchmarkGroup<WallTime>,
+	throughput_var: ThroughputVariant,
 	thread_pool: &ThreadPool,
 	num_threads: usize,
 	data: &mut [P],
@@ -45,11 +55,11 @@ fn bench_ntts<F, P>(
 		"threads={num_threads}/log_d={log_d}/log_x={log_x}/log_y={log_y}/log_z={log_z}/skip_rounds={skip_rounds}"
 	);
 
-	// We set the input size for the throughput to be the number of performed multiplications.
-	// This way, we can compare to the raw multiplication throughput (from the field benchmarks) and
-	// see the overhead of the NTT.
-	let num_muls = num_muls(shape, skip_rounds);
-	group.throughput(Throughput::Elements(num_muls));
+	let throughput = match throughput_var {
+		ThroughputVariant::Standard => Throughput::Bytes(std::mem::size_of_val(data) as u64),
+		ThroughputVariant::Multiplication => Throughput::Elements(num_muls(shape, skip_rounds)),
+	};
+	group.throughput(throughput);
 
 	let ntt = SingleThreadedNTT::<F>::new(log_y).unwrap();
 	group.bench_function(BenchmarkId::new("singlethread/on-the-fly/forward", &parameter), |b| {
@@ -90,11 +100,12 @@ fn bench_ntts<F, P>(
 }
 
 /// Calls `bench_ntts` with a fixed `PackedField` but different parameters.
-fn bench_params<F, P>(c: &mut Criterion<WallTimeMulThroughput>, packed_field_name: &str)
+fn bench_params<F, P>(c: &mut Criterion, packed_field_name: &str)
 where
 	F: BinaryField,
 	P: PackedField<Scalar = F>,
 {
+	let throughput_var = determine_throughput_variant();
 	let mut group = c.benchmark_group(packed_field_name);
 	let mut rng = rand::rng();
 
@@ -114,16 +125,16 @@ where
 				group.sample_size(50);
 			}
 
-			bench_ntts(&mut group, &thread_pool, num_threads, &mut data, 0, 0, 0);
-			bench_ntts(&mut group, &thread_pool, num_threads, &mut data, 4, 0, 0);
-			bench_ntts(&mut group, &thread_pool, num_threads, &mut data, 0, 4, 0);
-			bench_ntts(&mut group, &thread_pool, num_threads, &mut data, 0, 0, 4);
+			bench_ntts(&mut group, throughput_var, &thread_pool, num_threads, &mut data, 0, 0, 0);
+			bench_ntts(&mut group, throughput_var, &thread_pool, num_threads, &mut data, 4, 0, 0);
+			bench_ntts(&mut group, throughput_var, &thread_pool, num_threads, &mut data, 0, 4, 0);
+			bench_ntts(&mut group, throughput_var, &thread_pool, num_threads, &mut data, 0, 0, 4);
 		}
 	}
 }
 
 /// Calls `bench_params` with different fields.
-fn bench_fields(c: &mut Criterion<WallTimeMulThroughput>) {
+fn bench_fields(c: &mut Criterion) {
 	bench_params::<_, binius_field::PackedBinaryPolyval1x128b>(c, "1xPolyv");
 	bench_params::<_, binius_field::PackedBinaryPolyval2x128b>(c, "2xPolyv");
 	bench_params::<_, binius_field::PackedBinaryPolyval4x128b>(c, "4xPolyv");
@@ -144,111 +155,33 @@ fn num_muls(shape: NTTShape, skip_rounds: usize) -> u64 {
 	muls_single_ntt << log_batch_size
 }
 
-criterion_group! {
-	name = custom_formatter;
-	config = Criterion::default().with_measurement(WallTimeMulThroughput);
-	targets = bench_fields
-}
-criterion_main!(custom_formatter);
+/// Determine the throughput variant based on an environment variable.
+fn determine_throughput_variant() -> ThroughputVariant {
+	const VAR_NAME: &str = "NTT_MUL_THROUGHPUT";
 
-/// This implements a custom `Measurement`, which is just copy-paste from the standard `WallTime`
-/// (criterion version `0.6.0`), but uses `MultiplicationFormatter` as a formatter.
-struct WallTimeMulThroughput;
-
-impl Measurement for WallTimeMulThroughput {
-	type Intermediate = Instant;
-	type Value = Duration;
-
-	fn start(&self) -> Self::Intermediate {
-		Instant::now()
-	}
-
-	fn end(&self, i: Self::Intermediate) -> Self::Value {
-		i.elapsed()
-	}
-
-	fn add(&self, v1: &Self::Value, v2: &Self::Value) -> Self::Value {
-		*v1 + *v2
-	}
-
-	fn zero(&self) -> Self::Value {
-		Duration::from_secs(0)
-	}
-
-	fn to_f64(&self, val: &Self::Value) -> f64 {
-		val.as_nanos() as f64
-	}
-
-	fn formatter(&self) -> &dyn ValueFormatter {
-		&MultiplicationFormatter
-	}
-}
-
-struct MultiplicationFormatter;
-
-impl MultiplicationFormatter {
-	/// This is copy-paste from `DurationFormatter::elements_per_second` (criterion version
-	/// `0.6.0`), except that the strings are adapted
-	fn muls_per_second(&self, elems: f64, typical: f64, values: &mut [f64]) -> &'static str {
-		let elems_per_second = elems * (1e9 / typical);
-
-		let (denominator, unit) = if elems_per_second < 1000.0 {
-			(1.0, " mul/s")
-		} else if elems_per_second < 1000.0 * 1000.0 {
-			(1000.0, "Kmul/s")
-		} else if elems_per_second < 1000.0 * 1000.0 * 1000.0 {
-			(1000.0 * 1000.0, "Mmul/s")
-		} else {
-			(1000.0 * 1000.0 * 1000.0, "Gmul/s")
-		};
-
-		for val in values {
-			let elems_per_second = elems * (1e9 / *val);
-
-			*val = elems_per_second / denominator;
+	match std::env::var(VAR_NAME) {
+		Ok(val) => {
+			if val == "1" || val.to_lowercase() == "true" {
+				println!("Found {VAR_NAME}={val} - using MULTIPLICATION throughput");
+				ThroughputVariant::Multiplication
+			} else {
+				println!("Found {VAR_NAME}={val} - using STANDARD throughput");
+				ThroughputVariant::Standard
+			}
 		}
-
-		unit
+		Err(err) => match err {
+			VarError::NotPresent => {
+				println!(
+					"NOTE: If you want to use multiplication throughput instead of normal throughput, use {VAR_NAME}=1"
+				);
+				ThroughputVariant::Standard
+			}
+			VarError::NotUnicode(_) => {
+				panic!("Found non-unicode data in environment variable {VAR_NAME}");
+			}
+		},
 	}
 }
 
-/// This implementation is copy-paste from `DurationFormatter` (criterion version `0.6.0`), except
-/// that we only accept `Throughput::Elements` and call `muls_per_second(...)`.
-impl ValueFormatter for MultiplicationFormatter {
-	fn scale_throughputs(
-		&self,
-		typical: f64,
-		throughput: &Throughput,
-		values: &mut [f64],
-	) -> &'static str {
-		match *throughput {
-			Throughput::Elements(elems) => self.muls_per_second(elems as f64, typical, values),
-			_ => panic!("can only format elements/second throughput (not bytes)"),
-		}
-	}
-
-	fn scale_values(&self, ns: f64, values: &mut [f64]) -> &'static str {
-		let (factor, unit) = if ns < 10f64.powi(0) {
-			(10f64.powi(3), "ps")
-		} else if ns < 10f64.powi(3) {
-			(10f64.powi(0), "ns")
-		} else if ns < 10f64.powi(6) {
-			(10f64.powi(-3), "µs")
-		} else if ns < 10f64.powi(9) {
-			(10f64.powi(-6), "ms")
-		} else {
-			(10f64.powi(-9), "s")
-		};
-
-		for val in values {
-			*val *= factor;
-		}
-
-		unit
-	}
-
-	fn scale_for_machines(&self, _values: &mut [f64]) -> &'static str {
-		// no scaling is needed
-		"ns"
-	}
-}
+criterion_group!(default, bench_fields);
+criterion_main!(default);
