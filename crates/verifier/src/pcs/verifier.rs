fcad0af40f8fb6d97307a8c46577ee8b248a465b
Hunter Lindauer <holindauer@gmail.com>
2025-07-23 12:22:48 +0200

    fix

--- crates/verifier/src/pcs/verifier.rs
+++ crates/verifier/src/pcs/verifier.rs
@@ -1,106 +1,125 @@
 use binius_field::{BinaryField, ExtensionField, Field, PackedExtension, PackedField, TowerField};
-use binius_math::{multilinear::eq::eq_ind_partial_eval, tensor_algebra::TensorAlgebra};
+use binius_math::{
+	inner_product::inner_product, multilinear::eq::eq_ind_partial_eval,
+	tensor_algebra::TensorAlgebra,
+};
 use binius_transcript::{
 	VerifierTranscript,
 	fiat_shamir::{CanSample, Challenger},
 };
 use binius_utils::DeserializeBytes;
-use itertools::Itertools;
 
 use crate::{
-	fields::B1,
-	fri::FRIParams,
-	merkle_tree::MerkleTreeScheme,
-	protocols::basefold::verifier::{verify_final_basefold_assertion, verify_transcript},
+	fields::B1, fri::FRIParams, merkle_tree::MerkleTreeScheme,
+	protocols::basefold::verifier::verify_transcript as verify_basefold_transcript,
+	ring_switch::verifier::eval_rs_eq,
 };
 
-pub const PACKING_DEGREE: usize = 7;
+/// Verifies a ring switched pcs proof from the final transcript.
+///
+/// The verifier first checks the initial ring switching phase of the proof for
+/// completeness.Then, he verifies the underyling large field pcs using the
+/// basefold verifier.
+///
+/// ## Arguments
+///
+/// * `transcript` - the transcript of the prover's proof
+/// * `evaluation_claim` - the evaluation claim of the prover
+/// * `eval_point` - the evaluation point of the prover
+/// * `codeword_commitment` - VCS commitment to the codeword
+/// * `fri_params` - the FRI parameters
+/// * `vcs` - the VCS
+pub fn verify_transcript<F, FA, TranscriptChallenger, VCS>(
+	transcript: &mut VerifierTranscript<TranscriptChallenger>,
+	evaluation_claim: F,
+	eval_point: &[F],
+	codeword_commitment: VCS::Digest,
+	fri_params: &FRIParams<F, FA>,
+	vcs: &VCS,
+) -> Result<(), String>
+where
+	F: Field
+		+ BinaryField
+		+ PackedField<Scalar = F>
+		+ ExtensionField<FA>
+		+ TowerField
+		+ PackedExtension<B1>,
+	FA: BinaryField,
+	TranscriptChallenger: Challenger + Clone,
+	VCS: MerkleTreeScheme<F, Digest: DeserializeBytes>,
+{
+	let packing_degree = <F as ExtensionField<B1>>::LOG_DEGREE;
 
-pub struct OneBitPCSVerifier {}
+	// partial evals of packed mle at high degree vars
+	let s_hat_v = transcript
+		.message()
+		.read_scalar_slice::<F>(1 << packing_degree)
+		.expect("failed to read s_hat_v");
 
-impl OneBitPCSVerifier {
-	pub fn verify_transcript<F, FA, TranscriptChallenger, VCS>(
-		codeword_commitment: VCS::Digest,
-		transcript: &mut VerifierTranscript<TranscriptChallenger>,
-		evaluation_claim: F,
-		eval_point: &[F],
-		fri_params: &FRIParams<F, FA>,
-		vcs: &VCS,
-		n_vars: usize,
-	) -> Result<(), String>
-	where
-		F: Field
-			+ BinaryField
-			+ PackedField<Scalar = F>
-			+ ExtensionField<FA>
-			+ TowerField
-			+ PackedExtension<B1>,
-		FA: BinaryField,
-		TranscriptChallenger: Challenger + Clone,
-		VCS: MerkleTreeScheme<F, Digest: DeserializeBytes>,
-	{
-		// retrieve partial eval of t' at high degree variables
-		let s_hat_v = transcript
-			.message()
-			.read_scalar_slice::<F>(1 << PACKING_DEGREE)
-			.expect("failed to read s_hat_v");
+	// check initial message is partial eval
+	let (eval_point_low, _) = eval_point.split_at(packing_degree);
+	assert_eq!(
+		evaluation_claim,
+		inner_product::<F>(
+			s_hat_v.clone(),
+			eq_ind_partial_eval(eval_point_low)
+				.as_ref()
+				.into_iter()
+				.map(|x: &F| *x)
+		)
+	);
 
-		// 		// verifier checks initial message
-		// 		let (eval_point_low, _) = eval_point.split_at(KAPPA);
-		// 		assert_eq!(
-		// 			evaluation_claim,
-		// 			compute_mle_eq_sum(&s_hat_v, eq_ind_partial_eval(eval_point_low).as_ref())
-		// 		);
+	// basis decompose/recombine s_hat_v across opposite dimension
+	let s_hat_u: Vec<F> = <TensorAlgebra<B1, F>>::new(s_hat_v).transpose().elems;
 
-		// 		// basis decompose/recombine s_hat_v across opposite dimension
-		// 		let s_hat_u: Vec<BigField> = construct_bitsliced_claims::<B1, BigField>(s_hat_v);
+	// retrieve batching scalars
+	let batching_scalars: Vec<F> = verifier_samples_batching_scalars(transcript);
 
-		// 		// retrieve batching scalars
-		// 		let batching_scalars: Vec<BigField> =
-		// 			OneBitPCSVerifier::verifier_samples_batching_scalars(transcript);
+	let tensor_expanded_batching_scalars = eq_ind_partial_eval(&batching_scalars);
 
-		// 		let verifier_eq_r_double_prime = eq_ind_partial_eval(&batching_scalars);
+	// infer sumcheck claim from transcript
+	let verifier_computed_sumcheck_claim = inner_product::<F>(
+		s_hat_u,
+		tensor_expanded_batching_scalars
+			.as_ref()
+			.into_iter()
+			.map(|x: &F| *x),
+	);
 
-		// 		// infer sumcheck claim from transcript
-		// 		let verifier_computed_sumcheck_claim = compute_expected_sumcheck_claim::<B1, BigField>(
-		// 			&s_hat_u,
-		// 			verifier_eq_r_double_prime.as_ref(),
-		// 		);
+	// verify large field pcs w/ transcript
+	let (final_fri_oracle, sumcheck_final_claim, basefold_challenges) = verify_basefold_transcript(
+		codeword_commitment,
+		transcript,
+		verifier_computed_sumcheck_claim,
+		fri_params,
+		vcs,
+		eval_point.len() - packing_degree,
+	)
+	.expect("failed to verify basefold transcript");
 
-		// 		let (fri_final_value, sumcheck_final_claim, basefold_challenges) =
-		// 			BaseFoldVerifier::verify_transcript(
-		// 				codeword_commitment,
-		// 				transcript,
-		// 				verifier_computed_sumcheck_claim,
-		// 				fri_params,
-		// 				vcs,
-		// 				n_vars - KAPPA,
-		// 			)
-		// 			.expect("failed to verify basefold transcript");
+	// Final basefold verification
 
-		// 		// Final Basefold Verification
-		// 		let (_, eval_point_high) = eval_point.split_at(KAPPA);
+	let (_, eval_point_high) = eval_point.split_at(packing_degree);
 
-		// 		let rs_eq_at_basefold_challenges_verifier = eval_rs_eq(
-		// 			eval_point_high,
-		// 			&basefold_challenges,
-		// 			eq_ind_partial_eval(&batching_scalars).as_ref(),
-		// 		);
+	let rs_eq_at_basefold_challenges = eval_rs_eq(
+		eval_point_high,
+		&basefold_challenges,
+		eq_ind_partial_eval(&batching_scalars).as_ref(),
+	);
 
-		// 		assert_eq!(fri_final_value * rs_eq_at_basefold_challenges_verifier,
-		// sumcheck_final_claim);
+	assert_eq!(final_fri_oracle * rs_eq_at_basefold_challenges, sumcheck_final_claim);
 
-		Ok(())
-	}
+	Ok(())
+}
 
-	pub fn verifier_samples_batching_scalars<BigField: Field, TranscriptChallenger>(
-		transcript: &mut VerifierTranscript<TranscriptChallenger>,
-	) -> Vec<BigField>
-	where
-		TranscriptChallenger: Challenger,
-	{
-		(0..PACKING_DEGREE)
-			.map(|_| transcript.sample())
-			.collect_vec()
-	}
+/// Verifier samples batching scalars from the transcript
+pub fn verifier_samples_batching_scalars<F, FE, T>(
+	transcript: &mut VerifierTranscript<T>,
+) -> Vec<FE>
+where
+	F: Field,
+	FE: Field + ExtensionField<F> + PackedExtension<F>,
+	T: Challenger,
+{
+	(0..FE::LOG_DEGREE).map(|_| transcript.sample()).collect()
 }
