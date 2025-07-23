721f175c94a05e1d39f28aa7556f8b0517441f7c
Hunter Lindauer <holindauer@gmail.com>
2025-07-23 09:44:13 +0200

    add test + cleanup

--- crates/prover/src/ring_switch/prover.rs
+++ crates/prover/src/ring_switch/prover.rs
@@ -17,7 +17,7 @@ where
 
 	let row_batching_query = eq_ind_partial_eval::<FE>(batching_challenges);
 
-	let mut rs_eq_mle = FieldBuffer::<FE>::zeros(big_field_hypercube_vertices);
+	let mut rs_eq_mle = FieldBuffer::<FE>::zeros(z_vals.len());
 
 	for big_field_hypercube_vertex in 0..big_field_hypercube_vertices {
 		for (index, bit) in <FE as ExtensionField<F>>::into_iter_bases(
@@ -37,58 +37,85 @@ where
 
 #[cfg(test)]
 mod test {
-	use std::iter::repeat_with;
+	use super::rs_eq_ind;
 
-	use binius_field::{BinaryField1b, BinaryField128b, Field, Random};
-	use binius_math::multilinear::eq::eq_ind_partial_eval;
+	use std::iter::repeat_with;
+	use binius_field::{ExtensionField, BinaryField1b, BinaryField128b, Random};
+	use binius_math::{
+		FieldBuffer, 
+		multilinear::eq::eq_ind_partial_eval, 
+		inner_product::inner_product_packed,
+		test_utils::index_to_hypercube_point
+	};
 	use binius_verifier::ring_switch::verifier::eval_rs_eq;
 	use rand::{SeedableRng, rngs::StdRng};
 
-	use super::rs_eq_ind;
 
 	type F = BinaryField1b;
 	type FE = BinaryField128b;
 
-	fn hypercube_query<FE: Field + binius_field::ExtensionField<F>>(
-		idx: usize,
-		query_len: usize,
-	) -> Vec<FE> {
-		let mut result = vec![];
-
-		for i in 0..query_len {
-			if idx & 1 << i == 0 {
-				result.push(FE::ZERO);
-			} else {
-				result.push(FE::ONE);
-			}
-		}
-
-		result
-	}
-
 	#[test]
 	fn consistent_with_tensor_alg() {
-		let n_vars_big_field = 3;
 		let mut rng = StdRng::from_seed([0; 32]);
+
+		let n_vars_big_field = 3;
+
 		let z_vals: Vec<_> = repeat_with(|| FE::random(&mut rng))
 			.take(n_vars_big_field)
 			.collect();
 
-		let row_batching_challenges: Vec<_> =
-			repeat_with(|| FE::random(&mut rng)).take(7).collect();
+		let row_batching_challenges: Vec<FE> =
+			repeat_with(|| FE::random(&mut rng)).take(<FE as ExtensionField<F>>::LOG_DEGREE).collect();
 
 		let row_batching_expanded_query = eq_ind_partial_eval(&row_batching_challenges);
 
-		let mle = rs_eq_ind::<F, FE>(&row_batching_challenges, &z_vals);
+		let rs_eq = rs_eq_ind::<F, FE>(&row_batching_challenges, &z_vals);
 
 		for hypercube_point in 0..1 << 3 {
 			let evaluated_at_pt = eval_rs_eq::<F, FE>(
 				&z_vals,
-				&hypercube_query(hypercube_point, 3),
+				&index_to_hypercube_point::<FE>(3, hypercube_point),
 				row_batching_expanded_query.as_ref(),
 			);
 
-			assert_eq!(mle.get(hypercube_point).unwrap(), evaluated_at_pt);
+			assert_eq!(rs_eq.get(hypercube_point).unwrap(), evaluated_at_pt);
 		}
+	}
+
+	#[test]
+	fn test_out_of_range_evaluation() {
+		let mut rng = StdRng::from_seed([0; 32]);
+
+		let n_vars_big_field = 3;
+		
+		let z_vals: Vec<_> = repeat_with(|| FE::random(&mut rng))
+			.take(n_vars_big_field)
+			.collect();
+
+		let row_batching_challenges: Vec<FE> =
+			repeat_with(|| FE::random(&mut rng)).take(<FE as ExtensionField<F>>::LOG_DEGREE).collect();
+
+		let row_batching_expanded_query: FieldBuffer<FE> = eq_ind_partial_eval(&row_batching_challenges);
+
+		let rs_eq = rs_eq_ind::<F, FE>(&row_batching_challenges, &z_vals);
+
+		// out of range eval point
+		let eval_point: Vec<FE> =
+			repeat_with(|| FE::random(&mut rng)).take(n_vars_big_field).collect();
+
+		let tensor_expanded_eval_point = eq_ind_partial_eval::<FE>(&eval_point);
+
+		let expected_eval = inner_product_packed(
+			&rs_eq,
+			&tensor_expanded_eval_point,
+		);
+
+		let actual_eval = eval_rs_eq::<F, FE>(
+			&z_vals,
+			&eval_point,
+			row_batching_expanded_query.as_ref(),
+		);
+
+		assert_eq!(expected_eval, actual_eval);
 	}
 }
