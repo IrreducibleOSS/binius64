62347b8491c2a08c4de58a5c92a9d2c4e6e67ed6
Hunter Lindauer <holindauer@gmail.com>
2025-07-23 10:15:47 +0200

    doc string

--- crates/prover/src/ring_switch/prover.rs
+++ crates/prover/src/ring_switch/prover.rs
@@ -1,15 +1,31 @@
 use binius_field::{ExtensionField, Field};
 use binius_math::{FieldBuffer, multilinear::eq::eq_ind_partial_eval};
 
-/// Generate the ring switching equality indicator for a given query and z_vals
+/// Compute the multilinear extension of the ring switching equality indicator.
 ///
-/// Used by the prover within the the one bit pcs prover during the setup for the
-/// large field pcs.
+/// The ring switching equality indicator is the multilinear function $A$ from [DP24],
+/// Construction 3.1. Its multilinear extension is computed by basis decomposing the
+/// field extension elements of the tensor expanded z_vals point, then recombining
+/// the sub field basis elements with the large field tensor expanded row batching
+/// scalars.
+///
+/// ## Arguments
+///
+/// * `batching_challenges` - the scaling elements for row-batching
+/// * `z_vals` - the vertical evaluation point, with $\ell'$ components
+///
+/// ## Pre-conditions
+///
+/// * the length of batching challenges must equal `FE::LOG_DEGREE`
+///
+/// [DP24]: <https://eprint.iacr.org/2024/504>
 pub fn rs_eq_ind<F, FE>(batching_challenges: &[FE], z_vals: &[FE]) -> FieldBuffer<FE>
 where
 	F: Field,
 	FE: Field + binius_field::ExtensionField<F>,
 {
+	assert_eq!(batching_challenges.len(), FE::LOG_DEGREE);
+
 	let big_field_hypercube_vertices = 1 << z_vals.len();
 
 	// MLE-random-linear-combination of bit-slices of the eq_ind for z_vals
@@ -37,25 +53,23 @@ where
 
 #[cfg(test)]
 mod test {
-	use super::rs_eq_ind;
-
 	use std::iter::repeat_with;
-	use binius_field::{ExtensionField, BinaryField1b, BinaryField128b, Random};
+
+	use binius_field::{BinaryField1b, BinaryField128b, ExtensionField, Random};
 	use binius_math::{
-		FieldBuffer, 
-		multilinear::eq::eq_ind_partial_eval, 
-		inner_product::inner_product_packed,
-		test_utils::index_to_hypercube_point
+		FieldBuffer, inner_product::inner_product_packed, multilinear::eq::eq_ind_partial_eval,
+		test_utils::index_to_hypercube_point,
 	};
 	use binius_verifier::ring_switch::verifier::eval_rs_eq;
 	use rand::{SeedableRng, rngs::StdRng};
 
+	use super::rs_eq_ind;
 
 	type F = BinaryField1b;
 	type FE = BinaryField128b;
 
 	#[test]
-	fn consistent_with_tensor_alg() {
+	fn test_consistent_with_tensor_alg() {
 		let mut rng = StdRng::from_seed([0; 32]);
 
 		let n_vars_big_field = 3;
@@ -64,13 +78,15 @@ mod test {
 			.take(n_vars_big_field)
 			.collect();
 
-		let row_batching_challenges: Vec<FE> =
-			repeat_with(|| FE::random(&mut rng)).take(<FE as ExtensionField<F>>::LOG_DEGREE).collect();
+		let row_batching_challenges: Vec<FE> = repeat_with(|| FE::random(&mut rng))
+			.take(<FE as ExtensionField<F>>::LOG_DEGREE)
+			.collect();
 
 		let row_batching_expanded_query = eq_ind_partial_eval(&row_batching_challenges);
 
 		let rs_eq = rs_eq_ind::<F, FE>(&row_batching_challenges, &z_vals);
 
+		// test all points points in the boolean hypercube
 		for hypercube_point in 0..1 << 3 {
 			let evaluated_at_pt = eval_rs_eq::<F, FE>(
 				&z_vals,
@@ -87,34 +103,33 @@ mod test {
 		let mut rng = StdRng::from_seed([0; 32]);
 
 		let n_vars_big_field = 3;
-		
+
+		// setup ring switch eq mle
 		let z_vals: Vec<_> = repeat_with(|| FE::random(&mut rng))
 			.take(n_vars_big_field)
 			.collect();
 
-		let row_batching_challenges: Vec<FE> =
-			repeat_with(|| FE::random(&mut rng)).take(<FE as ExtensionField<F>>::LOG_DEGREE).collect();
+		let row_batching_challenges: Vec<FE> = repeat_with(|| FE::random(&mut rng))
+			.take(<FE as ExtensionField<F>>::LOG_DEGREE)
+			.collect();
 
-		let row_batching_expanded_query: FieldBuffer<FE> = eq_ind_partial_eval(&row_batching_challenges);
+		let row_batching_expanded_query: FieldBuffer<FE> =
+			eq_ind_partial_eval(&row_batching_challenges);
 
 		let rs_eq = rs_eq_ind::<F, FE>(&row_batching_challenges, &z_vals);
 
 		// out of range eval point
-		let eval_point: Vec<FE> =
-			repeat_with(|| FE::random(&mut rng)).take(n_vars_big_field).collect();
+		let eval_point: Vec<FE> = repeat_with(|| FE::random(&mut rng))
+			.take(n_vars_big_field)
+			.collect();
+
+		// compare eval against inner product w/ eq ind mle of eval point
 
 		let tensor_expanded_eval_point = eq_ind_partial_eval::<FE>(&eval_point);
+		let expected_eval = inner_product_packed(&rs_eq, &tensor_expanded_eval_point);
 
-		let expected_eval = inner_product_packed(
-			&rs_eq,
-			&tensor_expanded_eval_point,
-		);
-
-		let actual_eval = eval_rs_eq::<F, FE>(
-			&z_vals,
-			&eval_point,
-			row_batching_expanded_query.as_ref(),
-		);
+		let actual_eval =
+			eval_rs_eq::<F, FE>(&z_vals, &eval_point, row_batching_expanded_query.as_ref());
 
 		assert_eq!(expected_eval, actual_eval);
 	}
