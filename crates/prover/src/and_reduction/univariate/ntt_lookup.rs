750b7546828f00373b397ea8af99515c64f72081
jsk11235 <kirmayerjacob@gmail.com>
2025-07-24 10:05:31 +0200

    ntt lookup

--- crates/prover/src/and_reduction/univariate/ntt_lookup.rs
+++ crates/prover/src/and_reduction/univariate/ntt_lookup.rs
@@ -10,89 +10,111 @@ use binius_verifier::and_reduction::{
 	utils::constants::{ROWS_PER_HYPERCUBE_VERTEX, SKIPPED_VARS},
 };
 
-pub type NTTLookup<P> = Vec<Vec<Vec<P>>>;
+pub struct NTTLookup<P>(Vec<Vec<Vec<P>>>);
 
-// first index is the idx of the 8 bit chunk that we're in
-// second index is the bit string
-//
-// ASSUME: Each lagrange basis vector thats being ntt'ed has size 2^SKIPPED_VARS
-// ASSUME: Lagrange basis domain is simply AESTowerField8b(0..64)
-pub fn precompute_lookup<
-	PNTTDomain: PackedField<Scalar = FNTTDomain>,
-	FNTTDomain: BinaryField + Field + From<u8>,
->(
-	lexicographic_ntt_domain: &[FNTTDomain],
-) -> NTTLookup<PNTTDomain> {
-	let _span = tracing::debug_span!("precompute_lookup").entered();
+impl<PNTTDomain> NTTLookup<PNTTDomain>
+where
+	PNTTDomain: PackedField,
+	PNTTDomain::Scalar: BinaryField + Field + From<u8>,
+{
+	// first index is the idx of the 8 bit chunk that we're in
+	// second index is the bit string
+	//
+	// ASSUME: Each lagrange basis vector thats being ntt'ed has size 2^SKIPPED_VARS
+	// ASSUME: Lagrange basis domain is simply FNTTDomain(0..64)
+	pub fn new(lexicographic_ntt_domain: &[PNTTDomain::Scalar]) -> Self {
+		assert_eq!(PNTTDomain::WIDTH, 16);
+		let _span = tracing::debug_span!("precompute_lookup").entered();
 
-	let mut lookup =
-		vec![
+		let mut lookup =
 			vec![
-				vec![PNTTDomain::zero(); lexicographic_ntt_domain.len() / PNTTDomain::WIDTH];
-				1 << 8
+				vec![
+					vec![PNTTDomain::zero(); lexicographic_ntt_domain.len() / PNTTDomain::WIDTH];
+					1 << 8
+				];
+				ROWS_PER_HYPERCUBE_VERTEX / 8
 			];
-			ROWS_PER_HYPERCUBE_VERTEX / 8
-		];
-	let lagrange_basis_domain: Vec<_> = (0..ROWS_PER_HYPERCUBE_VERTEX)
-		.map(|x| FNTTDomain::from(x as u8))
-		.collect();
+		let lagrange_basis_domain: Vec<_> = (0..ROWS_PER_HYPERCUBE_VERTEX)
+			.map(|x| PNTTDomain::Scalar::from(x as u8))
+			.collect();
 
-	let mut eval_point_basis_point_to_numerator =
-		vec![vec![FNTTDomain::ZERO; lagrange_basis_domain.len()]; lexicographic_ntt_domain.len()];
-	let denominator: FNTTDomain = lexicographic_lagrange_denominator(SKIPPED_VARS);
+		let mut eval_point_basis_point_to_numerator =
+			vec![
+				vec![PNTTDomain::Scalar::ZERO; lagrange_basis_domain.len()];
+				lexicographic_ntt_domain.len()
+			];
+		let denominator: PNTTDomain::Scalar = lexicographic_lagrange_denominator(SKIPPED_VARS);
 
-	let inverse_denominator = denominator.invert_or_zero();
-	for (eval_point_idx, eval_point) in lexicographic_ntt_domain.iter().enumerate() {
-		eval_point_basis_point_to_numerator[eval_point_idx] =
-			lexicographic_lagrange_numerators_8b(ROWS_PER_HYPERCUBE_VERTEX, *eval_point);
-	}
-
-	for eight_bit_chunk_idx in 0..ROWS_PER_HYPERCUBE_VERTEX / 8 {
-		for log_coefficient_as_bit_string in 0..8 {
-			let coefficient_as_bit_string = 1 << log_coefficient_as_bit_string;
-			let nonzero_lagrange_basis_coeffs: Vec<_> =
-				PackedBinaryField8x1b::from_underlier(coefficient_as_bit_string)
-					.iter()
-					.collect();
-			let mut lagrange_basis_coeffs = [BinaryField1b::ZERO; ROWS_PER_HYPERCUBE_VERTEX];
-
-			for (i, nonzero_lagrange_basis_coeff) in
-				nonzero_lagrange_basis_coeffs.into_iter().enumerate()
-			{
-				lagrange_basis_coeffs[eight_bit_chunk_idx * 8 + i] = nonzero_lagrange_basis_coeff;
-			}
-
-			#[allow(clippy::needless_range_loop)]
-			for eval_point_idx in 0..lexicographic_ntt_domain.len() {
-				let mut result = FNTTDomain::ZERO;
-				for basis_point_idx in 0..lagrange_basis_domain.len() {
-					result += (eval_point_basis_point_to_numerator[eval_point_idx]
-						[basis_point_idx] * lagrange_basis_coeffs[basis_point_idx])
-						* inverse_denominator;
-				}
-				set_packed_slice(
-					&mut lookup[eight_bit_chunk_idx][coefficient_as_bit_string as usize],
-					eval_point_idx,
-					result,
-				);
-			}
+		let inverse_denominator = denominator.invert_or_zero();
+		for (eval_point_idx, eval_point) in lexicographic_ntt_domain.iter().enumerate() {
+			eval_point_basis_point_to_numerator[eval_point_idx] =
+				lexicographic_lagrange_numerators_8b(ROWS_PER_HYPERCUBE_VERTEX, *eval_point);
 		}
-	}
 
-	for this_byte_lookup in lookup.iter_mut() {
-		for coefficient_as_bit_string in 0..1 << 8 {
-			let mut result =
-				vec![PNTTDomain::zero(); lexicographic_ntt_domain.len() / PNTTDomain::WIDTH];
-			for bit_in_string in 0..8 {
-				let this_one_hot = coefficient_as_bit_string & 1 << bit_in_string;
-				for (i, result_packed_elem) in result.iter_mut().enumerate() {
-					*result_packed_elem += this_byte_lookup[this_one_hot][i];
+		for eight_bit_chunk_idx in 0..ROWS_PER_HYPERCUBE_VERTEX / 8 {
+			for log_coefficient_as_bit_string in 0..8 {
+				let coefficient_as_bit_string = 1 << log_coefficient_as_bit_string;
+				let nonzero_lagrange_basis_coeffs: Vec<_> =
+					PackedBinaryField8x1b::from_underlier(coefficient_as_bit_string)
+						.iter()
+						.collect();
+				let mut lagrange_basis_coeffs = [BinaryField1b::ZERO; ROWS_PER_HYPERCUBE_VERTEX];
+
+				for (i, nonzero_lagrange_basis_coeff) in
+					nonzero_lagrange_basis_coeffs.into_iter().enumerate()
+				{
+					lagrange_basis_coeffs[eight_bit_chunk_idx * 8 + i] =
+						nonzero_lagrange_basis_coeff;
+				}
+
+				#[allow(clippy::needless_range_loop)]
+				for eval_point_idx in 0..lexicographic_ntt_domain.len() {
+					let mut result = PNTTDomain::Scalar::ZERO;
+					for basis_point_idx in 0..lagrange_basis_domain.len() {
+						result += (eval_point_basis_point_to_numerator[eval_point_idx]
+							[basis_point_idx] * lagrange_basis_coeffs[basis_point_idx])
+							* inverse_denominator;
+					}
+					set_packed_slice(
+						&mut lookup[eight_bit_chunk_idx][coefficient_as_bit_string as usize],
+						eval_point_idx,
+						result,
+					);
 				}
 			}
-			this_byte_lookup[coefficient_as_bit_string] = result;
 		}
+
+		for this_byte_lookup in lookup.iter_mut() {
+			for coefficient_as_bit_string in 0..1 << 8 {
+				let mut result =
+					vec![PNTTDomain::zero(); lexicographic_ntt_domain.len() / PNTTDomain::WIDTH];
+				for bit_in_string in 0..8 {
+					let this_one_hot = coefficient_as_bit_string & 1 << bit_in_string;
+					for (i, result_packed_elem) in result.iter_mut().enumerate() {
+						*result_packed_elem += this_byte_lookup[this_one_hot][i];
+					}
+				}
+				this_byte_lookup[coefficient_as_bit_string] = result;
+			}
+		}
+		NTTLookup(lookup)
 	}
-	lookup
+
+	#[inline]
+	pub fn ntt(
+		&self,
+		coeffs_in_byte_chunks: impl Iterator<Item = u8>,
+	) -> [PNTTDomain; ROWS_PER_HYPERCUBE_VERTEX / 16] {
+		let mut result = [PNTTDomain::zero(); ROWS_PER_HYPERCUBE_VERTEX / 16];
+
+		for (eight_bit_chunk_idx, eight_bit_chunk) in coeffs_in_byte_chunks.enumerate() {
+			for j in 0..ROWS_PER_HYPERCUBE_VERTEX / 16 {
+				result[j] += self.0[eight_bit_chunk_idx][eight_bit_chunk as usize][j];
+			}
+		}
+
+		result
+	}
 }
 
 #[cfg(test)]
@@ -102,8 +124,7 @@ mod test {
 		packed::get_packed_slice,
 	};
 
-	use super::ROWS_PER_HYPERCUBE_VERTEX;
-	use crate::and_reduction::univariate::ntt_lookup::precompute_lookup;
+	use super::{NTTLookup, ROWS_PER_HYPERCUBE_VERTEX};
 
 	#[test]
 	fn assert_accurate_ntt_on_well_known_poly() {
@@ -111,9 +132,10 @@ mod test {
 			.map(|x| AESTowerField8b::new(x as u8))
 			.collect();
 
-		let lookup = precompute_lookup::<PackedAESBinaryField16x8b, _>(&output_domain);
+		let lookup = NTTLookup::new(&output_domain);
 
-		let results = lookup[0][1].clone();
+		let slice_to_ntt: [u8; _] = [1, 0, 0, 0, 0, 0, 0, 0];
+		let results: [PackedAESBinaryField16x8b; _] = lookup.ntt(slice_to_ntt.into_iter());
 
 		for (i, input) in output_domain.iter().enumerate() {
 			let expected_result = (1..ROWS_PER_HYPERCUBE_VERTEX)
