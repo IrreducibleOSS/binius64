72c2a635a8b2d1f491c2785eabc4728fe8af6565
jimpo <881253+jimpo@users.noreply.github.com>
2025-08-05 11:34:28 +0000

    [examples] Make sha256 example do a full SHA-256 hash (#427)

--- crates/examples/examples/sha256.rs
+++ crates/examples/examples/sha256.rs
@@ -1,11 +1,10 @@
-use binius_core::word::Word;
+use std::array;
+
 use binius_examples::{prove_verify, setup};
-use binius_frontend::{
-	circuits::sha256::{Compress, State},
-	compiler,
-	compiler::Wire,
-};
+use binius_frontend::{circuits::sha256::Sha256, compiler, compiler::Wire};
 use clap::Parser;
+use rand::prelude::*;
+use sha2::Digest;
 
 #[derive(Parser, Debug)]
 #[command(name = "sha256")]
@@ -16,37 +15,32 @@ struct Args {
 	log_inv_rate: u32,
 }
 
+fn mk_circuit(b: &mut compiler::CircuitBuilder, max_n: usize, len: Wire) -> Sha256 {
+	let digest: [Wire; 4] = array::from_fn(|_| b.add_inout());
+	let n_blocks = (max_n + 9).div_ceil(64);
+	let n_words = n_blocks * 8;
+	let message = (0..n_words).map(|_| b.add_inout()).collect();
+	Sha256::new(b, max_n, len, digest, message)
+}
+
 fn main() {
 	let args = Args::parse();
 	let _tracing_guard = tracing_profile::init_tracing().unwrap();
 
+	let mut rng = StdRng::seed_from_u64(0);
+
+	const EXACT_LEN: usize = 2048;
+
+	let mut message = vec![0u8; EXACT_LEN];
+	rng.fill_bytes(&mut message);
+	let digest = sha2::Sha256::digest(&message);
+
 	let build_scope = tracing::info_span!("Building circuit").entered();
 
-	// Use the test-vector for SHA256 single block message: "abc".
-	let mut preimage: [u8; 64] = [0; 64];
-	preimage[0..3].copy_from_slice(b"abc");
-	preimage[3] = 0x80;
-	preimage[63] = 0x18;
-
-	#[rustfmt::skip]
-	let expected_state: [u32; 8] = [
-		0xba7816bf, 0x8f01cfea, 0x414140de, 0x5dae2223,
-		0xb00361a3, 0x96177a9c, 0xb410ff61, 0xf20015ad,
-	];
-
-	let circuit = compiler::CircuitBuilder::new();
-	let state = State::iv(&circuit);
-	let input: [Wire; 16] = std::array::from_fn(|_| circuit.add_witness());
-	let output: [Wire; 8] = std::array::from_fn(|_| circuit.add_inout());
-	let compress = Compress::new(&circuit, state, input);
-
-	// Mask to only low 32-bit.
-	let mask32 = circuit.add_constant(Word::MASK_32);
-	for (actual_x, expected_x) in compress.state_out.0.iter().zip(output) {
-		circuit.assert_eq("eq", circuit.band(*actual_x, mask32), expected_x);
-	}
-
-	let circuit = circuit.build();
+	let mut builder = compiler::CircuitBuilder::new();
+	let len = builder.add_constant_64(EXACT_LEN as u64);
+	let sha256_gadget = mk_circuit(&mut builder, EXACT_LEN, len);
+	let circuit = builder.build();
 	drop(build_scope);
 
 	let log_inv_rate = args.log_inv_rate as usize;
@@ -56,12 +50,11 @@ fn main() {
 	let witness = tracing::info_span!("Generating witness").in_scope(|| {
 		let mut w = circuit.new_witness_filler();
 
-		// Populate the input message for the compression function.
-		compress.populate_m(&mut w, preimage);
+		// Populate the input message for the hash function.
+		sha256_gadget.populate_len(&mut w, EXACT_LEN);
+		sha256_gadget.populate_message(&mut w, &message);
+		sha256_gadget.populate_digest(&mut w, digest.into());
 
-		for (i, &output) in output.iter().enumerate() {
-			w[output] = Word(expected_state[i] as u64);
-		}
 		circuit.populate_wire_witness(&mut w).unwrap();
 		w.into_value_vec()
 	});
