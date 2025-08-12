1146f049561bfbe4201ddcabf2760fbd1c6af444
Jim Posen <jimpo@irreducible.com>
2025-08-12 09:33:11 +0200

    [inverse_or_zero_mle] Improve docs

--- crates/prover/src/protocols/sumcheck/inverse_or_zero_mle.rs
+++ crates/prover/src/protocols/sumcheck/inverse_or_zero_mle.rs
@@ -1,4 +1,4 @@
-// Copyright 2023-2025 Irreducible Inc.
+// Copyright 2025 Irreducible Inc.
 
 use std::array;
 
@@ -10,63 +10,25 @@ use binius_verifier::protocols::sumcheck::RoundCoeffs;
 use super::{common::SumcheckProver, error::Error, gruen34::Gruen34};
 use crate::protocols::sumcheck::common::MleCheckProver;
 
-/// A [`SumcheckProver`] implementation that reduces an evaluation claim on a multilinear extension
-/// of the composition $A^2 \cdot B - A$ to evaluation claims on the constituent multilinears.
-/// We call such reductions Mlechecks.
+/// An [`MleCheckProver`] implementation for the composition $(A \cdot B - 1) \cdot A$.
+///
+/// This composite arises in the reduction of zero-check claims that $A$'s evaluations are equal to
+/// the multiplicative inverse of $B$'s evaluations, but additionally mapping 0 to 0.
+/// zero-check claims that  to evaluation claims on the constituent multilinears.
 ///
 /// ## Mathematical Definition
 /// * $n \in \mathbb{N}$ - number of variables in multilinear polynomials
 /// * $A, B \in \mathbb{F}\[x\]$, $x = (x_1, \ldots, x_n)$ - input multilinears
 /// * $(A^2B - A)(x) = y$ - evaluation claim on the composition MLE
-///
-/// The claim is equivalent to $P(x) = \sum_{v \in \{0,1\}^n} \widetilde{eq}(v, x) (A(v)^2 B(v) -
-/// A(v)) = y$, and the reduction can be achieved by sumchecking this degree-4 composition. The
-/// paper [Gruen24], however, describes a way to partition the $\widetilde{eq}(v, x)$ into three
-/// parts in round $j \in 1, \ldots, n$ during specialization of variable $v_{n-j+1}$, with $j-1$
-/// challenges $\alpha_i$ already sampled:
-///
-/// $$ \widetilde{eq}(x_{n-j+2}, \ldots, x_n; \alpha_{j-1}, \ldots, \alpha_{1}) \tag{1} $$
-/// $$ \widetilde{eq}(x_{n-j+1}; v_{n-j+1}) \tag{2} $$
-/// $$ \widetilde{eq}(x_1, \ldots, x_{n-j}; v_1, \ldots, v_{n-j}) \tag{3} $$
-///
-/// The following holds:
-/// * (1) is a constant that can be incrementally updated in O(1) time,
-/// * (2) is a linear polynomial that is easy to compute in monomial form specialized to either
-///   variable
-/// * (3) is an equality indicator over the claim point suffix
-///
-/// These observations allow us to instead sumcheck:
-/// $$
-/// P'(x) = \sum_{v \in \{0,1\}^n} \widetilde{eq}(x_1, \ldots, x_{n-j}; v_1, \ldots, v_{n-j})
-/// (A(v)^2 B(v) - A(v)) $$
-///
-/// Which is simpler because:
-/// * $P'(x)$ is degree-3 in $j$-th variable (since $A^2 \cdot B - A$ has degree 3), requiring one
-///   less evaluation point
-/// * Equality indicator expansion does not depend on $j$-th variable and thus doesn't need to be
-///   interpolated
-///
-/// After computing the round polynomial for $P'(x)$ in monomial form, one can simply multiply by
-/// (2) and (1) in polynomial form. For more details, see [`Gruen34`]('gruen34::Gruen34') struct and
-/// [Gruen24] Section 3.4.
-///
-/// Note 1: as evident from the definition, this prover binds variables in high-to-low index order.
-///
-/// Note 2: evaluation points are 0 (implicit), 1 and Karatsuba infinity.
-///
-/// # Invariants
-///
-/// - The length of both multilinears is always equal.
-///
-/// [Gruen24]: <https://eprint.iacr.org/2024/108>
 #[derive(Debug, Clone)]
-pub struct XSquaredYMinusXMlecheckProver<P: PackedField> {
+pub struct InverseOrZeroMleCheckProver<P: PackedField> {
+	/// Invariant: the length of both multilinears is always equal.
 	multilinears: [FieldBuffer<P>; 2],
 	last_coeffs_or_eval: RoundCoeffsOrEval<P::Scalar>,
 	gruen34: Gruen34<P>,
 }
 
-impl<F: Field, P: PackedField<Scalar = F>> XSquaredYMinusXMlecheckProver<P> {
+impl<F: Field, P: PackedField<Scalar = F>> InverseOrZeroMleCheckProver<P> {
 	/// Constructs a prover, given the multilinear polynomial evaluations and the evaluation
 	/// claim on the multilinear extension of their composition $A^2 \cdot B - A$.
 	pub fn new(
@@ -94,7 +56,7 @@ impl<F: Field, P: PackedField<Scalar = F>> XSquaredYMinusXMlecheckProver<P> {
 	}
 }
 
-impl<F, P> SumcheckProver<F> for XSquaredYMinusXMlecheckProver<P>
+impl<F, P> SumcheckProver<F> for InverseOrZeroMleCheckProver<P>
 where
 	F: Field,
 	P: PackedField<Scalar = F>,
@@ -213,7 +175,7 @@ where
 	}
 }
 
-impl<F, P> MleCheckProver<F> for XSquaredYMinusXMlecheckProver<P>
+impl<F, P> MleCheckProver<F> for InverseOrZeroMleCheckProver<P>
 where
 	F: Field,
 	P: PackedField<Scalar = F>,
@@ -386,7 +348,7 @@ mod tests {
 	}
 
 	#[test]
-	fn test_x2y_minus_x_mlecheck() {
+	fn test_inverse_or_zero_mlecheck() {
 		type F = OptimalB128;
 		type P = OptimalPackedB128;
 
@@ -407,7 +369,7 @@ mod tests {
 		let eval_claim = evaluate(&composition_buffer, &eval_point).unwrap();
 
 		// Create the prover
-		let mlecheck_prover = XSquaredYMinusXMlecheckProver::new(
+		let mlecheck_prover = InverseOrZeroMleCheckProver::new(
 			[multilinear_a.clone(), multilinear_b.clone()],
 			&eval_point,
 			eval_claim,
