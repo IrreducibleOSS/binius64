88ae738b7778815c71c667bd13b5fcf709240b4c
Johann Birnick <johann.birnick@hotmail.de>
2025-08-21 10:20:18 +0200

    bitcoin circuits beginning

---
+++ crates/frontend/src/circuits/bitcoin/mod.rs
@@ -0,0 +1,5 @@
+//! Circuits related to Bitcoin.
+
+pub mod block_contains_transaction;
+pub mod double_sha256;
+pub mod merkle_path;
--- crates/frontend/src/circuits/mod.rs
+++ crates/frontend/src/circuits/mod.rs
@@ -1,5 +1,6 @@
 pub mod base64;
 pub mod bignum;
+pub mod bitcoin;
 pub mod blake2b;
 pub mod concat;
 pub mod ecdsa;
