e074e8c3a2d5ebc6e51bd10996a749b89d6306c9
danilo <danilo.sijacic@icloud.com>
2025-09-03 17:08:03 +0200

    fix CI

--- crates/examples/examples/hashsign.rs
+++ crates/examples/examples/hashsign.rs
@@ -1,8 +1,5 @@
 use anyhow::Result;
-use binius_examples::{
-	Cli,
-	circuits::hashsign::{HashBasedSigExample, Instance, Params},
-};
+use binius_examples::{Cli, circuits::hashsign::HashBasedSigExample};
 
 fn main() -> Result<()> {
 	let _tracing_guard = tracing_profile::init_tracing()?;
