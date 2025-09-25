6de9ee2003ecd521fdd32b67863479353194b64d
Jim Posen <jimpo@irreducible.com>
2025-09-25 21:18:24 +0200

    Some progres on optimizer

--- verifier/spartan-frontend/src/r1cs.rs
+++ verifier/spartan-frontend/src/r1cs.rs
@@ -1,13 +1,11 @@
 // Copyright 2025 Irreducible Inc.
 
-use std::collections::BTreeMap;
-
 use binius_field::{BinaryField128bGhash as B128, Field};
-use smallvec::SmallVec;
+use smallvec::{SmallVec, smallvec};
 
-use crate::{AddConstraint, ConstraintWire, WitnessIndex};
+use crate::{AddConstraint, ConstraintWire, MulConstraint, WireKind, WitnessIndex};
 
-struct MulConstraint {
+struct OperandMulConstraint {
 	a: Operand,
 	b: Operand,
 	c: Operand,
@@ -15,16 +13,88 @@ struct MulConstraint {
 
 struct Operand(SmallVec<[ConstraintWire; 4]>);
 
+impl From<ConstraintWire> for Operand {
+	fn from(value: ConstraintWire) -> Self {
+		Operand(smallvec![value])
+	}
+}
+
+#[derive(Debug, Clone)]
+enum UseSite {
+	Add { index: u32 },
+	Mul { index: u32 },
+}
+
+#[derive(Debug, Clone)]
+enum WireStatus {
+	Pinned,
+	Pruned,
+	Unknown { uses: Vec<UseSite> },
+}
+
+impl Default for WireStatus {
+	fn default() -> Self {
+		WireStatus::Unknown { uses: Vec::new() }
+	}
+}
+
 pub struct R1CS {
-	witness_size: u32,
 	constants: Vec<B128>,
-	// TODO: This can just be a vec with binary search, BTreeMap not necessary.
-	index_map: BTreeMap<ConstraintWire, WitnessIndex>,
+	// // TODO: This can just be a vec with binary search, BTreeMap not necessary.
+	// index_map: BTreeMap<ConstraintWire, WitnessIndex>,
 	add_constraints: Vec<Operand>,
-	mul_constraints: Vec<MulConstraint>,
+	mul_constraints: Vec<OperandMulConstraint>,
+	// Values are indices with WireKind::Private
+	private_wires: Vec<WireStatus>,
 }
 
 impl R1CS {
+	pub fn new(
+		n_constant: usize,
+		n_public: usize,
+		n_private: usize,
+		constants: Vec<B128>, //HashMap<B128, u32>,
+		add_constraints: Vec<AddConstraint>,
+		mul_constraints: Vec<MulConstraint>,
+	) -> Self {
+		let mut private_wires = vec![WireStatus::default(); n_private];
+
+		let mut r1cs_add_constraints = Vec::with_capacity(add_constraints.len());
+		for (i, AddConstraint(term)) in add_constraints.into_iter().enumerate() {
+			for wire in &term {
+				if matches!(wire.kind, WireKind::Private) {
+					if let WireStatus::Unknown { ref mut uses } = private_wires[wire.id as usize] {
+						uses.push(UseSite::Add { index: i as u32 });
+					}
+				}
+			}
+			r1cs_add_constraints.push(Operand(term));
+		}
+
+		let mut r1cs_mul_constraints = Vec::with_capacity(mul_constraints.len());
+		for (i, MulConstraint { a, b, c }) in mul_constraints.into_iter().enumerate() {
+			for wire in [a, b, c] {
+				if matches!(wire.kind, WireKind::Private) {
+					if let WireStatus::Unknown { ref mut uses } = private_wires[wire.id as usize] {
+						uses.push(UseSite::Mul { index: i as u32 });
+					}
+				}
+			}
+			r1cs_mul_constraints.push(OperandMulConstraint {
+				a: a.into(),
+				b: b.into(),
+				c: c.into(),
+			});
+		}
+
+		Self {
+			constants,
+			add_constraints: r1cs_add_constraints,
+			mul_constraints: r1cs_mul_constraints,
+			private_wires,
+		}
+	}
+
 	fn optimize_pass(&mut self) {
 		// map each private wire to its use defs
 		// identify candidates:
