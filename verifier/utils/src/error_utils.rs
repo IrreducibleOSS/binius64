e79c21de0bba63ffee444eaea6e6f81774970b99
Jim Posen <jimpo@irreducible.com>
2025-09-26 18:00:04 +0200

    [utils] Remove the bail_panic feature

--- verifier/utils/src/error_utils.rs
+++ verifier/utils/src/error_utils.rs
@@ -1,14 +1,5 @@
 // Copyright 2024-2025 Irreducible Inc.
 
-#[cfg(feature = "bail_panic")]
-#[macro_export]
-macro_rules! bail {
-	($err:expr) => {
-		panic!("{}", $err);
-	};
-}
-
-#[cfg(not(feature = "bail_panic"))]
 #[macro_export]
 macro_rules! bail {
 	($err:expr) => {
